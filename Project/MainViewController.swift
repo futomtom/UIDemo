e6193becae1f852a400a46577ed751a20fd08fb2
alexfu <afu@dynosense.com>
2017-02-19 22:20:16 -0800

    1

---
+++ Project/MainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainVCViewController.swift
+//  Project
+//
+//  Created by alexfu on 2/19/17.
+//  Copyright © 2017 aau. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+    @IBAction func DoPay(_ sender: Any) {
+        touchIDAuth()
+        
+    }
+    
+    func touchIDAuth(){
+        //检查Touch ID是否可用
+        let authenticationContext = LAContext()
+        var error: NSError?
+        let isTouch = authenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,error: &error)
+        if isTouch{
+            //获取指纹验证结果
+            authenticationContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "需要验证您的指纹来确认您的身份信息", reply: {
+                (success, error) -> Void in
+                if success{
+                    print("恭喜，您通过了Touch ID指纹验证！")
+                }else{
+                    print("抱歉，您未能通过Touch ID指纹验证！\n\(error)")
+                }
+            })
+        }else{
+            print("抱歉，Touch ID不可以使用！\n\(error)")
+        }
+    }
+
+}
---
+++ Project/ScanBarCodeViewController.swift
@@ -0,0 +1,97 @@
+
+import UIKit
+import AVFoundation
+
+class ScanBarCodeViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var label: UILabel!
+    
+    let detectionArea = UIView()
+    var timer: Timer!
+    var counter = 0
+    var isDetected = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     
+        let captureSession = AVCaptureSession()
+        
+      
+        let videoDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        let videoInput = try! AVCaptureDeviceInput.init(device: videoDevice)
+        captureSession.addInput(videoInput)
+
+             let metadataOutput = AVCaptureMetadataOutput()
+        captureSession.addOutput(metadataOutput)
+        
+     
+        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+       
+        metadataOutput.metadataObjectTypes = [AVMetadataObjectTypeEAN13Code,AVMetadataObjectTypeEAN8Code]
+        
+    
+        let x: CGFloat = 0.05
+        let y: CGFloat = 0.3
+        let width: CGFloat = 0.9
+        let height: CGFloat = 0.2
+        
+        detectionArea.frame = CGRect(x: view.frame.size.width * x, y: view.frame.size.height * y, width: view.frame.size.width * width, height: view.frame.size.height * height)
+        detectionArea.layer.borderColor = UIColor.red.cgColor
+        detectionArea.layer.borderWidth = 3
+        view.addSubview(detectionArea)
+
+      
+        metadataOutput.rectOfInterest = CGRect(x: y,y: 1-x-width,width: height,height: width)
+        
+ 
+        if let videoLayer = AVCaptureVideoPreviewLayer.init(session: captureSession) {
+            videoLayer.frame = previewView.bounds
+            videoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            previewView.layer.addSublayer(videoLayer)
+        }
+        
+ 
+        DispatchQueue.global(qos: .userInitiated).async {
+            captureSession.startRunning()
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        timer.fire()
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+    
+        for metadata in metadataObjects as! [AVMetadataMachineReadableCodeObject] {
+        
+            if metadata.type == AVMetadataObjectTypeEAN13Code || metadata.type == AVMetadataObjectTypeEAN8Code{
+                if metadata.stringValue != nil {
+               
+                    counter = 0
+                    if !isDetected || label.text != metadata.stringValue! {
+                        isDetected = true
+                        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate) // バイブレーション
+                        label.text = metadata.stringValue!
+                        detectionArea.layer.borderColor = UIColor.white.cgColor
+                        detectionArea.layer.borderWidth = 5
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func update(tm: Timer) {
+        counter += 1
+        print(counter)
+        if 1 < counter {
+            detectionArea.layer.borderColor = UIColor.red.cgColor
+            detectionArea.layer.borderWidth = 3
+            label.text = ""
+        }
+    }
+}
+
