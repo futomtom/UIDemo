6ed0f1506e806e0241ec8ac11c57a55b837a3814
Alex <afu@dynosense.com>
2017-03-19 21:54:15 -0700

    1

---
+++ Project/ExpensePopVC.swift
@@ -0,0 +1,35 @@
+//
+//  MainVC.swift
+//  Project
+//
+//  Created by siqi on 2/23/17.
+//  Copyright © 2017 aau. All rights reserved.
+//
+
+import UIKit
+
+class ExpensePopVC: UIViewController {
+
+  
+    var setting = Setting.share
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.tintColor = UIColor(red:0.659,  green:0.792,  blue:0.812, alpha:1)
+       
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        title = ""
+    }
+
+
+    @IBAction func closedidTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
