d77972ee68eeb7020bb1be74dee5aa5a81e5b9e7
alexfu <afu@dynosense.com>
2017-03-25 22:28:52 -0700

    1

--- Project/ExpensePopVC.swift
+++ Project/ExpensePopVC.swift
@@ -15,10 +15,32 @@ class ExpensePopVC: UIViewController {
     let names = ["Rent","Groceries","Eating Out","Utilities","Cellphone","Car","Fun","Costume","Travel","Household","Cosmetics","Healthcare","Gifts","Social","Investment","Other"]
     var name = ""
     var previousButton:UIButton?
+    
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button10: UIButton!
+    @IBOutlet weak var button11: UIButton!
+    @IBOutlet weak var button12: UIButton!
+    @IBOutlet weak var button13: UIButton!
+    @IBOutlet weak var button14: UIButton!
+    @IBOutlet weak var button15: UIButton!
+
+    var buttons:[UIButton] = []
+    
   
     var setting = Setting.share
     override func viewDidLoad() {
         super.viewDidLoad()
+         buttons = [button0,button1,button2,button3,button4,
+                                 button5,button6,button7,button8,button9,button10,button11,button12,button13,button14,button15]
         
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
@@ -33,7 +55,7 @@ class ExpensePopVC: UIViewController {
     }
     
     @IBAction func CategoryDidSelected(_ sender: UIButton) {
-        
+        buttons.map ({ $0.isSelected = false })
         sender.isSelected = true
         name = names[sender.tag]
         
